fcfc5a00096404787cd78b0013899f68542a3967
manishpython <manishsaxena2007@gmail.com>
2019-04-18 13:20:58 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
  <?php 
- echo "I am VERSION 1 <br><br>";
- echo "My Pod IP is : ".$_SERVER['SERVER_ADDR'] ;
+ echo "Application VERSION 1 <br><br>";
+ echo "Running on Pod IP is : ".$_SERVER['SERVER_ADDR'] ;
  ?>
