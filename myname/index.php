7a4874887ae37b512e11cc6cb92d1fbb2c279b62
root <root@master1.ffc3.internal>
2019-05-31 07:06:00 +0000

    test3

--- myname/index.php
+++ myname/index.php
@@ -1,4 +1,4 @@
  <?php 
  echo "Application VERSION 2 <br><br>";
- echo "Running on Pod IP is GREEN POD : ".$_SERVER['SERVER_ADDR'] ;
+ echo "Running on GREEN POD with IP : ".$_SERVER['SERVER_ADDR'] ;
  ?>
